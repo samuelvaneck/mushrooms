deefac24038fa3b09e8ded1139732934ab981199
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-07-11 17:55:08 +0200

    Add RSpec test

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -4,30 +4,31 @@ class Mushroom < ApplicationRecord
   def self.import(file)
     counter = 0
     File.open(file).each do |line|
+      m_array = line.gsub("\n", '').split(',')
       mushroom = Mushroom.new(
-        edible: line[0] == 'e',
-        cap_shape: line[1],
-        cap_surface: line[2],
-        cap_color: line[3],
-        bruises: line[4] == 't',
-        odor: line[5],
-        gill_attachment: line[6],
-        gill_spacing: line[7],
-        gill_size: line[8],
-        gill_color: line[9],
-        stalk_shape: line[10],
-        stalk_root: line[11],
-        stalk_surface_above_ring: line[12],
-        stalk_surface_below_ring: line[13],
-        stalk_color_above_ring: line[14],
-        stalk_color_below_ring: line[15],
-        veil_type: line[16],
-        veil_color: line[17],
-        ring_number: line[18],
-        ring_type: line[19],
-        spore_print_color_range: line[20],
-        population: line[21],
-        habitat: line[22]
+        edible: m_array[0] == 'e',
+        cap_shape: m_array[1],
+        cap_surface: m_array[2],
+        cap_color: m_array[3],
+        bruises: m_array[4] == 't',
+        odor: m_array[5],
+        gill_attachment: m_array[6],
+        gill_spacing: m_array[7],
+        gill_size: m_array[8],
+        gill_color: m_array[9],
+        stalk_shape: m_array[10],
+        stalk_root: m_array[11],
+        stalk_surface_above_ring: m_array[12],
+        stalk_surface_below_ring: m_array[13],
+        stalk_color_above_ring: m_array[14],
+        stalk_color_below_ring: m_array[15],
+        veil_type: m_array[16],
+        veil_color: m_array[17],
+        ring_number: m_array[18],
+        ring_type: m_array[19],
+        spore_print_color_range: m_array[20],
+        population: m_array[21],
+        habitat: m_array[22]
       )
 
       counter += 1 if mushroom.save
--- spec/models/mushroom_spec.rb
+++ spec/models/mushroom_spec.rb
@@ -1,7 +1,49 @@
-# fronzen_string_literal: true
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe Mushroom, type: :model do
-  
+  describe '#import' do
+    context 'with valid data' do
+      before { @file = file_fixture('test_import.data') }
+      it 'imports the data' do
+        expect do
+          Mushroom.import(@file)
+        end.to change(Mushroom, :count).by(5)
+      end
+
+      it 'returns the number of imported objects' do
+        expect(Mushroom.import(@file)).to eq 5
+      end
+
+      it 'import the mushroom with correct values' do
+        Mushroom.import(@file)
+        mushroom = Mushroom.order(:created_at).first
+
+        expect(mushroom.edible).to eq false
+        expect(mushroom.cap_shape).to eq 'x'
+        expect(mushroom.cap_surface).to eq 's'
+        expect(mushroom.cap_color).to eq 'n'
+        expect(mushroom.bruises).to eq true
+        expect(mushroom.odor).to eq 'p'
+        expect(mushroom.gill_attachment).to eq 'f'
+        expect(mushroom.gill_spacing).to eq 'c'
+        expect(mushroom.gill_size).to eq 'n'
+        expect(mushroom.gill_color).to eq 'k'
+        expect(mushroom.stalk_shape).to eq 'e'
+        expect(mushroom.stalk_root).to eq 'e'
+        expect(mushroom.stalk_surface_above_ring).to eq 's'
+        expect(mushroom.stalk_surface_below_ring).to eq 's'
+        expect(mushroom.stalk_color_above_ring).to eq 'w'
+        expect(mushroom.stalk_color_below_ring).to eq 'w'
+        expect(mushroom.veil_type).to eq 'p'
+        expect(mushroom.veil_color).to eq 'w'
+        expect(mushroom.ring_number).to eq 'o'
+        expect(mushroom.ring_type).to eq 'p'
+        expect(mushroom.spore_print_color_range).to eq 'k'
+        expect(mushroom.population).to eq 's'
+        expect(mushroom.habitat).to eq 'u'
+      end
+    end
+  end
 end
