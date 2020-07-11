38c3d69d4720c310137b3c46a5ea52bb9a178e2e
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-07-11 18:27:58 +0200

    Add model constant

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
 class Mushroom < ApplicationRecord
+
+  CAP_SHAPE = [['bell', 'b'], ['conical', 'c'], ['convex', 'x'], ['flat', 'f'], ['knobbed', 'k'], ['sunken', 's']].freeze
+  CAP_SURFACE = [['fibrous', 'f'], ['grooves', 'g'], ['scaly', 'y'], ['smooth', 's']].freeze
+  CAP_COLOR = [['brown', 'n'], ['buff', 'b'] , ['cinnamon', 'c'], ['gray', 'g'], ['green', 'r'], ['pink', 'p'], ['purple', 'u'], ['red', 'e'], ['white', 'w'], ['yellow', 'y']].freeze
+  BRUISES = [['bruises', 't'], ['no', 'f']].freeze
+  ODOR = [['almond', 'a'], ['anise', 'l'], ['creosote', 'c'], ['fishy', 'y'], ['foul', 'f'], ['musty', 'm'], ['none', 'n'], ['pungent', 'p'], ['spicy', 's']].freeze
+  GILL_ATTACHMENT = [['attached', 'a'], ['descending', 'd'], ['free', 'f'], ['notched', 'n']].freeze
+  GILL_SPACINT = [['close', 'c'], ['crowded', 'w'], ['distant', 'd']].freeze
+  GILL_SIZE = [['broad', 'b'], ['narrow', 'n']].freeze
+  GILL_COLOR = [['black', 'k'],'brown', 'n'], ['buff', 'b'], ['chocolate', 'h'], ['gray', 'g'], ['green', 'r'], ['orange', 'o'], ['pink', 'p'], ['purple', 'u'], ['red', 'e'],  ['white', 'w'], ['yellow', 'y'].freeze
+  STALK_SHAPE = [['enlarging', 'e'], ['tapering', 't']]
+  STALK_ROOT = [['bulbous', 'b'], ['club', 'c'], ['cup', 'u'], ['equal', 'e'], [' rhizomorphs', 'z'], ['rooted', 'r'], ['missing', '?']].freeze
+  STALK_SURFACE_ABOVE_RING = [['fibrous, f'], ['scaly, y'], ['silky, k'], ['smooth, s']].freeze
+  STALK_SURFACE_BELOW_RING = [['fibrous, f'], ['scaly, y'], ['silky, k'], ['smooth, s']].freeze
+  STALK_COLOR_ABOVE_RING = [['brown', 'n'],['buff', 'b'], ['cinnamon', 'c'], ['gray', 'g'], ['orange', 'o'], ['pink', 'p'], ['red', 'e'], ['white', 'w'], ['yellow', 'y']].freeze
+  STALK_COLOR_BELOW_RING = [['brown', 'n'],['buff', 'b'], ['cinnamon', 'c'], ['gray', 'g'], ['orange', 'o'], ['pink', 'p'], ['red', 'e'], ['white', 'w'], ['yellow', 'y']].freeze
+  VEIL_TYPE = [['partial', 'p'], ['universal', 'u']].freeze
+  VEIL_COLOR = [['brown', 'n'], ['orange', 'o'], ['white', 'w'], ['yellow', 'y']].freeze
+  RING_NUMBER =[['none', 'n'], ['one', 'o'], ['two', 't']].freeze
+  RING_TYPE = [['cobwebby', 'c'], ['evanescent', 'e'], ['flaring', 'f'], ['large', 'l'], [' none', 'n'], ['pendant', 'p'], ['sheathing', 's'], ['zone', 'z']].freeze
+  SPORE_PRINT_COLOR = ['black', 'k', ['brown', 'n'], ['buff', 'b'], ['chocolate', 'h'], ['green', 'r'], ['orange', 'o'], ['purple', 'u'], ['white', 'w'], ['yellow', 'y']].freeze
+  POPULATION = [['abundant', 'a'], ['clustered', 'c'], ['numerous', 'n'], [' scattered', 's'], ['several', 'v'], ['solitary', 'y']].freeze
+  HABITAT = [['grasses', 'g'], ['leaves', 'l'], ['meadows', 'm'], ['paths', 'p'], ['urban', 'u'], ['waste', 'w'], ['woods', 'd']].freeze
+
   def self.import(file)
     counter = 0
     File.open(file).each do |line|
