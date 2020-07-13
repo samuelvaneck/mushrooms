7f59e671b90dca61b60f35d40a3f0a14102db928
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-07-13 17:44:20 +0200

    Add checkbox filter helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
   def convert_attr_value(const_str, value)
     Mushroom.const_get(const_str)[value.to_sym]
   end
+
+  def form_multiple_checkbox(const_str)
+    const_h = Mushroom.const_get(const_str)
+    content = ''
+
+    content += tag.label const_str.humanize
+    const_h.map do |value, text|
+      content += tag.input(type: :checkbox, name: const_str.downcase, value: value) +
+                 tag.label(text.humanize, for: const_str.downcase)
+    end
+
+    sanitize content, tags: ['input', 'label'], attributes: ['type', 'name', 'value', 'for']
+  end
 end
