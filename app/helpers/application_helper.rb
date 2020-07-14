738847e33ac82b234210eddeaa2350cb5693946d
Samuel van Eck <samuel.van.eck@simbeyond.com>
2020-07-14 06:26:18 +0200

    UI fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,13 @@ module ApplicationHelper
     const_h = Mushroom.const_get(const_str)
     attr_name = const_str.downcase
     content = ''
-    
+
     const_h.map do |value, text|
-      content += tag.input(type: :checkbox, name: attr_name + [].to_s, value: value) +
+      checked = params[attr_name].present? && (params[attr_name].include?(value.to_s) || value.to_s == '1')
+      content += tag.input(type: :checkbox, name: attr_name + [].to_s, value: value, checked: checked) +
                  tag.label(text.humanize, for: attr_name)
     end
 
-    sanitize content, tags: ['input', 'label'], attributes: ['type', 'name', 'value', 'for']
+    sanitize content, tags: ['input', 'label'], attributes: ['type', 'name', 'value', 'for', 'checked']
   end
 end
